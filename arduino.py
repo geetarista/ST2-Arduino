267e57c4bd00e3a6b768cb3130d566d2817cc10b
Robby Colvin <geetarista@gmail.com>
2012-05-10 18:44:47 -0700

    Starting to look more like a plugin

--- arduino.py
+++ arduino.py
@@ -1,4 +1,14 @@
-import os
-import subprocess
 import sublime
 import sublime_plugin
+
+import os
+import thread
+import subprocess
+
+class NewSketchCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    template = open(sublime.packages_path() + '/Arduino/sketch_template', 'r').read()
+    file = self.window.new_file()
+    edit = file.begin_edit()
+    file.insert(edit, 0, template)
+    file.end_edit(edit)
